9a2864f86197b4da9a772a436531d14d0313cf39
FabricioKenobi <fabriciojoel99@gmail.com>
2025-05-24 21:32:42 -0300

    cambios menores

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from api.views import login_view, register_view,home_view, crear_nota_view
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,9 +12,9 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/", include("api.urls")),
     path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
     path('', home_view, name='home'),  # Landing page después del login
     path('crear-nota/', crear_nota_view, name='crear_nota'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
