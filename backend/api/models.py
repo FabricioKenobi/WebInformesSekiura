fcb46841825dfeeb23802441ea972845467e62ab
FabricioKenobi <fabriciojoel99@gmail.com>
2025-06-04 21:28:51 -0300

    mejora de informe

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,35 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Nota(models.Model):
-    TIPO_CHOICES = [
-        ('diario', 'diario'),
-        ('mensual', 'mensual'),
-    ]
-
-    titulo = models.CharField(max_length=200)
-    contenido = models.TextField()
-    tipo = models.CharField(max_length=20, choices=TIPO_CHOICES)
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.titulo} ({self.tipo})"
-    
-class mail(models.Model):
-    ASUNTO_CHOICES = [
-        ('reunion', 'Reunión'),
-        ('informe', 'Informe'),
-        ('recordatorio', 'Recordatorio'),
-    ]
-
-    asunto = models.CharField(max_length=100, choices=ASUNTO_CHOICES)
-    fecha_evento = models.DateField()
-    cuerpo = models.TextField()
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    enviado = models.BooleanField(default=False)  # opcional, si querés trackearlo
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
     email_1 = models.EmailField(null=True, blank=True)
