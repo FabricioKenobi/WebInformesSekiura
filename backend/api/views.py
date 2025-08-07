282ba45b4bffb4d7ce1246499701ac50c83ce8cd
FabricioKenobi <fabriciojoel99@gmail.com>
2025-08-07 17:19:51 -0300

    prueba

--- backend/api/views.py
+++ backend/api/views.py
@@ -680,6 +680,7 @@ def ejecutar_comando_cliente(request):
     output_path = os.path.join(output_dir, nombre)
     nombre_rm = nombre.replace(' ', '\ ')
     output_path_rm = os.path.join(output_dir, nombre_rm)
+    print(informe)
     informe = informe.replace("%3A","\'")
     informe = informe.replace("%2C",",")
     print(informe)
